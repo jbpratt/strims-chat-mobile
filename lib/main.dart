4faeef5e8ae1226eb784d1e3514ff448ab839fd8
AliveSurfin <42511980+aliveSurfin@users.noreply.github.com>
2019-08-08 04:14:31 +0100

    Settings (#49): Settings persist throughout the app, settings.toggles store on change. added functions for rest of settings. Re positioned settings functions into settings class.
    * settings working up and down tree TODO: fix settings widget position in tree
    
    * update versions in pubspec.lock
    
    * change notifier changed to top of app tree.
    settings save and load toggles from storage added to settings widget.
    returned storage functions to futures
    
    Co-authored-by: jbpratt <jbpratt78@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
+import 'settings.dart';
 import 'chat.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-
+  Settings settings = new Settings();
   @override
-  Widget build(BuildContext context) { // TODO: add routes https://flutter.dev/docs/cookbook/navigation/navigate-with-arguments#3-register-the-widget-in-the-routes-table
-    return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Colors.grey[800],
-        accentColor: Colors.orange[700],
-      ),
-      home: new ChatPage(),
-    );
+  Widget build(BuildContext context) {
+    //
+    return ChangeNotifierProvider(
+        builder: (context) => SettingsNotifier(settings),
+        child: MaterialApp(
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            primaryColor: Colors.grey[800],
+            accentColor: Colors.orange[700],
+          ),
+          home: new ChatPage(),
+        ));
   }
 }
